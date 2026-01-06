c418ac6238c34c07434bd6ae08d1c3dc0f484fd2
kapiszektech1 <kontofn11@vp.pl>
2026-01-06 18:12:50 +0100

    Update powitania.js

--- powitania.js
+++ powitania.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     createWelcomeEmbed: (member) => {
         // Dodajemy zabezpieczenie: jeśli nie uda się pobrać liczby osób, pokaże "wielu"
-        const memberCount = member.guild?.member_count || member.guild?.approximateMemberCount || "wielu";
+        const memberCount = member.guild?.memberCount || "wielu";
         const currentDate = new Date().toLocaleDateString('pl-PL');
 
         return new EmbedBuilder()
@@ -16,7 +16,9 @@ module.exports = {
                 { name: '📅 Data', value: `\`${currentDate}\``, inline: true },
                 { name: '📈 Jesteś', value: `${memberCount} osobą`, inline: true }
             )
-            .setThumbnail(member.user.displayAvatarURL())
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+            // TUTAJ WKLEJ LINK DO SWOJEGO OBRAZKA (np. z Imgur)
+            .setImage('https://cdn.discordapp.com/attachments/1458122275973890222/1458146242608632034/Gemini_Generated_Image_uq8kmeuq8kmeuq8k.png?ex=695e9403&is=695d4283&hm=f009e95d74398d893cef686a462c77e5ed5dd50b781e611662886e456fc0dfc5') 
             .setFooter({ text: 'REP VAULT' });
     }
 };
