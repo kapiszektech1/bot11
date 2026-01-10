849db8924330ca98a8f9d11c4628af16f028e289
kapiszektech1 <kontofn11@vp.pl>
2026-01-10 21:23:53 +0100

    Update zaproszenia.js

--- zaproszenia.js
+++ zaproszenia.js
@@ -2,23 +2,29 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     createLuxuryInviteEmbed: (member, inviter) => {
+        // Obliczanie wieku konta
         const accountAgeDays = Math.floor((Date.now() - member.user.createdTimestamp) / (1000 * 60 * 60 * 24));
         
+        // Link do banera (Upewnij się, że link jest stały!)
+        const BANNER_URL = 'https://cdn.discordapp.com/attachments/1458122275973890222/1458152143050641601/image.png';
+
         return new EmbedBuilder()
             .setTitle('🔵 VAULT REP x NOWE ZAPROSZENIE')
-            .setDescription('**Informacje o koncie:**')
-            .setColor(0x00008B) // Kolor: Ciemny Niebieski (Dark Blue)
-            // Miniaturka: Zdjęcie profilowe nowego członka
-            .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 256 }))
+            .setDescription('**Właśnie dołączył nowy członek społeczności!**')
+            .setColor(0x00008B) // Luxury Dark Blue
+            .setThumbnail(member.user.displayAvatarURL({ forceStatic: false, size: 256 }))
             .addFields(
-                { name: '👤 Użytkownik:', value: `\`${member.user.username}\``, inline: false },
-                { name: 'ℹ️ Info o Koncie:', value: `\n(Wiek: ${accountAgeDays} dni)`, inline: false },
-                { name: '📩 Zaproszony przez:', value: inviter ? `\`${inviter.tag}\`` : '`Nieznane / Link Stały`', inline: false },
-                { name: '🎟️ Kupony:', value: '[Kliknij!](https://ikako.vip/r/xhm44)', inline: false }
+                { name: '👤 Użytkownik:', value: `${member.user}`, inline: true },
+                { name: '📅 Wiek konta:', value: `\`${accountAgeDays} dni\``, inline: true },
+                { name: '📩 Zaproszony przez:', value: inviter ? `**${inviter.tag}**` : '`Link Stały / Nieznane`', inline: false },
+                { name: '🎟️ Specjalne Kupony:', value: '[Odbierz zniżki tutaj!](https://ikako.vip/r/xhm44)', inline: false }
             )
-            // Twój baner na dole
-            .setImage('https://cdn.discordapp.com/attachments/1458122275973890222/1458152143050641601/image.png?ex=695e9982&is=695d4802&hm=66fd9f8bb29a49c4c45aefad2112b62dcc716f093106ff9a4d25db02f14649b3') 
-            .setFooter({ text: `VAULT REP • #${member.guild.memberCount} osoba` })
+            // UŻYWAMY IMAGE DLA BANERA
+            .setImage(BANNER_URL)
+            .setFooter({ 
+                text: `VAULT REP • Jesteśmy w składzie ${member.guild.memberCount} osób!`,
+                iconURL: member.guild.iconURL() 
+            })
             .setTimestamp();
     }
 };
